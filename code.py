4ade8a7822cdd2b2c4cbed1ddbf849ac8085b51f
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2018-12-07 13:42:27 +0530

    Modified oops code

--- code.py
+++ code.py
@@ -28,6 +28,8 @@ from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 import lightgbm as lgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 
 warnings.filterwarnings('ignore')
 
@@ -662,46 +664,149 @@ class HousePrices(object):
 		self.rmse= np.sqrt(-cross_val_score(model, self.train.values, self.y_train, scoring="neg_mean_squared_error", cv = self.kf))
 		return(self.rmse)
 
-	#Lasso
+	#Lasso. Best alpha : 0.0005 / 91% accuracy
 	def lasso_model(self):
-		#self.lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
-		#self.score = self.rmsle_cv(self.lasso)
-		self.score = self.rmsle_cv(HousePrices.lasso)
-		print("\nLasso score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+		self.lasso_m = Lasso()
+		self.alpha = [0.0005,0.0003,0.0007]
+		self.param_grid = dict(alpha=self.alpha)
+		self.grid_search = GridSearchCV(self.lasso_m, self.param_grid, scoring="r2",cv=10)
+		self.grid_result = self.grid_search.fit(self.train, self.y_train)
+		print("Best: %f using %s" % (self.grid_result.best_score_, self.grid_result.best_params_))
+		self.lasso = self.grid_search.best_estimator_
+		# #self.lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+		# #self.score = self.rmsle_cv(self.lasso)
+		# self.score = self.rmsle_cv(HousePrices.lasso)
+		# print("\nLasso score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))	
+		
 
-	# ElasticNet.
+	# ElasticNet. Best Alpha : 0.001  / 91% accuracy.
 	def elasticNet(self):
-		#self.ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
-		self.score = self.rmsle_cv(HousePrices.ENet)
-		print("ElasticNet score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+		self.enet_m = ElasticNet()
+		self.alpha = [0.0005,0.0007,0.001]
+		self.param_grid = dict(alpha=self.alpha)
+		self.grid_search = GridSearchCV(self.enet_m, self.param_grid, scoring="r2",cv=10)
+		self.grid_result = self.grid_search.fit(self.train, self.y_train)
+		print("Best: %f using %s" % (self.grid_result.best_score_, self.grid_result.best_params_))
+		self.enet_m = self.grid_search.best_estimator_
 
-	#Kernel Ridge regression.
+		# #self.ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+		# self.score = self.rmsle_cv(HousePrices.ENet)
+		# print("ElasticNet score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+
+	#Kernel Ridge regression. Best alpha : .0005 / 79% accuracy
 	def kernelRegression(self):
-		#self.KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
-		self.score = self.rmsle_cv(HousePrices.KRR)	
-		print("Kernel Ridge score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+		self.krr_m = KernelRidge()
+		self.alpha = [0.0005,0.0007,0.001,0.0006,0.0001]
+		self.param_grid = dict(alpha=self.alpha)
+		self.grid_search = GridSearchCV(self.krr_m, self.param_grid, scoring="r2",cv=10)
+		self.grid_result = self.grid_search.fit(self.train, self.y_train)
+		print("Best: %f using %s" % (self.grid_result.best_score_, self.grid_result.best_params_))
+		self.krr_m = self.grid_search.best_estimator_
 
-	#GradientBoosting.
+
+
+		# #self.KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+		# self.score = self.rmsle_cv(HousePrices.KRR)	
+		# print("Kernel Ridge score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+
+	#GradientBoosting. Best alpha : .00065 / 89% accuracy
 	def gradientBoosting(self):
-		#self.GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05, max_depth=4, max_features='sqrt', min_samples_leaf=15, min_samples_split=10,loss='huber', random_state =5)
-		self.score = self.rmsle_cv(HousePrices.GBoost)
-		print("Gradient Boosting score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+		self.gboost_m =  GradientBoostingRegressor()
+		self.alpha = [0.00068,0.00065,0.00066]
+		self.param_grid = dict(alpha=self.alpha)
+		self.grid_search = GridSearchCV(self.gboost_m, self.param_grid, scoring="r2",cv=10)
+		self.grid_result = self.grid_search.fit(self.train, self.y_train)
+		print("Best: %f using %s" % (self.grid_result.best_score_, self.grid_result.best_params_))
+		self.krr_m = self.grid_search.best_estimator_
 
-	# XgbRegressor.
+
+
+
+
+		# #self.GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05, max_depth=4, max_features='sqrt', min_samples_leaf=15, min_samples_split=10,loss='huber', random_state =5)
+		# self.score = self.rmsle_cv(HousePrices.GBoost)
+		# print("Gradient Boosting score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
+
+	# XgbRegressor.Best alpha : .0005 / 79% accuracy
 	def xgbRegressor(self):
 		#self.model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468,learning_rate=0.05, max_depth=3,min_child_weight=1.7817, n_estimators=2200,reg_alpha=0.4640, reg_lambda=0.8571,subsample=0.5213, silent=1,random_state =7, nthread = -1)
 		self.score = self.rmsle_cv(HousePrices.model_xgb)
 		print("Xgboost score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
 
-	# LgbRegressor.
+	# LgbRegressor. Best alpha : .0005 / 79% accuracy
 	def lgbRegressor(self):
 		#model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,learning_rate=0.05, n_estimators=720,max_bin = 55, bagging_fraction = 0.8,bagging_freq = 5, feature_fraction = 0.2319,feature_fraction_seed=9, bagging_seed=9, min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
 		self.score = self.rmsle_cv(HousePrices.model_lgb)
 		print("LgbRegressor score: {:.4f} ({:.4f})\n".format(self.score.mean(), self.score.std()))
 
+	def rmsle(self,y, y_pred):
+		return np.sqrt(mean_squared_error(y, y_pred))
+
+	def stackingModels(self):
+		#Lasso
+		self.lasso_stacking = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+		#ElasticNet
+		self.ENet_stacking = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+		#Kernel Ridge regression
+		self.KRR_stacking = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5) 
+		#GBoost
+		self.GBoost_stacking = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05, max_depth=4, max_features='sqrt', min_samples_leaf=15, min_samples_split=10,loss='huber', random_state =5)
+
+
+		#Lgb
+		self.lgb_stacking = lgb.LGBMRegressor(objective='regression',num_leaves=5,learning_rate=0.05, n_estimators=720,max_bin = 55, bagging_fraction = 0.8,bagging_freq = 5, feature_fraction = 0.2319,feature_fraction_seed=9, bagging_seed=9, min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+
+		#Stacking
+		self.stacked_averaged_models = StackingAveragedModels(base_models = (self.ENet_stacking,self.GBoost_stacking, self.KRR_stacking), meta_model = self.lasso_stacking)
+
+		self.score = self.rmsle_cv(self.stacked_averaged_models)
+		print("Stacking Averaged models score: {:.4f} ({:.4f})".format(self.score.mean(), self.score.std()))
+
+		self.stacked_averaged_models.fit(self.train.values, self.y_train)
+		self.stacked_train_pred = self.stacked_averaged_models.predict(self.train.values)
+		self.stacked_pred = np.expm1(self.stacked_averaged_models.predict(self.test.values))
+		print("RMSE of stacked ")
+		print(self.rmsle(self.y_train, self.stacked_train_pred))
 
 
 
+
+
+
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+   
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+        
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+                
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+   
+    #Do the predictions of all base models on the test data and use the averaged predictions as 
+    #meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_ ])
+        return self.meta_model_.predict(meta_features)
 
 
 if __name__ == "__main__":
@@ -714,10 +819,17 @@ if __name__ == "__main__":
 
 	model.preProcess()
 
+	#model.stackingModels()
+
+
 	#model.lasso_model()
+
+
 	#model.elasticNet()
 
-	# model.gradientBoosting()
+	#model.kernelRegression()
+
+	model.gradientBoosting()
 	# model.xgbRegressor()
 
 	# model.lgbRegressor()
